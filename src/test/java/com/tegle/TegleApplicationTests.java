59104447628609385f8be2ad54a9eee7f4061598
tjkim84 <56573782+tjkim84@users.noreply.github.com>
2021-08-18 16:59:50 +0900

    Add files via upload

---
+++ src/test/java/com/tegle/TegleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tegle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TegleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
